27255544ca017203f29d2b91996b69f5f6f85706
Louis Lebrault <louis.lebrault@gmail.com>
2025-01-02 19:32:24 +0100

    about page

---
+++ app/about/page.js
@@ -0,0 +1,24 @@
+export default function About() {
+  return (
+    <main>
+      <h2>About KanapéShop</h2>
+      <p>
+        Welcome to <strong>KanapéShop</strong>, your online destination for finding the perfect sofa that combines
+        style, comfort, and quality.
+      </p>
+      <p>
+        At KanapéShop, we understand that your sofa is more than just a piece of furniture. It's where you unwind after
+        a long day, share precious moments with your loved ones, and create lasting memories. That's why we're
+        committed to offering you a carefully curated selection of sofas that meet your needs and desires.
+      </p>
+      <h2>Our Mission</h2>
+      <p>
+        At KanapéShop, our mission is simple: to provide you with a seamless, enjoyable, and reliable online shopping
+        experience while offering the best sofas on the market.
+      </p>
+      <p>
+        Thank you for trusting KanapéShop. We look forward to helping you create your ideal space.
+      </p>
+    </main>
+  )
+}
