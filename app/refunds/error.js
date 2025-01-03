f1fe70ed261469ddd2c3fd3b0d701106eccd476d
Louis Lebrault <louis.lebrault@gmail.com>
2025-01-03 16:14:19 +0100

    add refunds error page

---
+++ app/refunds/error.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function RefundsError() {
+  return (
+    <main>
+      <h1>An unexpected error occured.</h1>
+      <p className="tableflip">щ(ºДºщ)</p>
+    </main>
+  );
+}
