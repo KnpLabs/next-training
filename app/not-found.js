7db075c8e66a60a7304f10f68255e05f12177b9e
Louis Lebrault <louis.lebrault@gmail.com>
2025-01-03 16:04:07 +0100

    not-found page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main>
+      <h1>The requested page could not be found...</h1>
+      <p className="tableflip">(╯°□°)╯︵ ┻━┻</p>
+    </main>
+  );
+};
