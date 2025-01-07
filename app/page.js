aa2bc5f2e8dfa0961f38b7e65641af7224b06f58
Louis Lebrault <louis.lebrault@gmail.com>
2025-01-07 14:43:01 +0100

    display best-sellers on home

--- app/page.js
+++ app/page.js
@@ -1,10 +1,23 @@
-export default function Home() {
+export default async function Home () {
+  const bestSellers = await fetch('http://localhost:8000/best-sellers')
+    .then(res => res.json());
+
   return (
     <main>
       <section>
         <h2>Best sellers</h2>
         <div className="home-products">
-          <p>Will be implemented soon...</p>
+          <ul>
+            {bestSellers.map(bestSeller => (
+              <li>
+                <article>
+                  <img src={bestSeller.image} alt="" />
+                  <h3>{bestSeller.name}</h3>
+                  <p>{bestSeller.price}</p>
+                </article>
+              </li>
+            ))}
+          </ul>
         </div>
       </section>
       <section>
