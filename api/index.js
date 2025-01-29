742f3471f63859ec32fa815c54b0e2115ab8758b
Louis Lebrault <louis.lebrault@gmail.com>
2025-01-29 11:13:40 +0100

    add products large images

--- api/index.js
+++ api/index.js
@@ -1,36 +1,108 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require("express");
+const app = express();
+const port = 8000;
 
-app.get('/best-sellers', (req, res) => {
+app.get("/best-sellers", (req, res) => {
   res.json([
-    { id: 1, "name": "Le Marquis", "image": "http://localhost:3000/le-marquis.jpg", "price": "8000€" },
-    { id: 2, "name": "Nuage", "image": "http://localhost:3000/nuage.jpg", "price": "700€" },
-    { id: 3, "name": "Boëme", "image": "http://localhost:3000/boeme.jpg", "price": "3000€" },
-    { id: 4, "name": "Obsidian", "image": "http://localhost:3000/obsidian.jpg", "price": "5320€" },
-  ])
-})
+    {
+      id: 1,
+      name: "Le Marquis",
+      imageSmall: "http://localhost:3000/le-marquis_small.jpg",
+      imageLarge: "http://localhost:3000/le-marquis_large.jpg",
+      price: "8000€",
+    },
+    {
+      id: 2,
+      name: "Nuage",
+      imageSmall: "http://localhost:3000/nuage_small.jpg",
+      imageLarge: "http://localhost:3000/nuage_large.jpg",
+      price: "700€",
+    },
+    {
+      id: 3,
+      name: "Boëme",
+      imageSmall: "http://localhost:3000/boeme_small.jpg",
+      imageLarge: "http://localhost:3000/boeme_large.jpg",
+      price: "3000€",
+    },
+    {
+      id: 4,
+      name: "Obsidian",
+      imageSmall: "http://localhost:3000/obsidian_small.jpg",
+      imageLarge: "http://localhost:3000/obsidian_large.jpg",
+      price: "5320€",
+    },
+  ]);
+});
 
-app.get('/on-sales', (req, res) => {
+app.get("/on-sales", (req, res) => {
   setTimeout(() => {
     res.json([
-      { id: 5,"name": "Bobby", "image": "http://localhost:3000/bobby.jpg", "price": "300€" },
-      { id: 6,"name": "Arlequin", "image": "http://localhost:3000/arlequin.jpg", "price": "650€" },
-      { id: 7,"name": "Highlander", "image": "http://localhost:3000/highlander.jpg", "price": "3200€" },
-      { id: 8,"name": "Prestige", "image": "http://localhost:3000/prestige.jpg", "price": "9800€" }
-    ])
-  }, 3000)
-})
+      {
+        id: 5,
+        name: "Bobby",
+        imageSmall: "http://localhost:3000/bobby_small.jpg",
+        imageLarge: "http://localhost:3000/bobby_large.jpg",
+        price: "300€",
+      },
+      {
+        id: 6,
+        name: "Arlequin",
+        imageSmall: "http://localhost:3000/arlequin_small.jpg",
+        imageLarge: "http://localhost:3000/arlequin_large.jpg",
+        price: "650€",
+      },
+      {
+        id: 7,
+        name: "Highlander",
+        imageSmall: "http://localhost:3000/highlander_small.jpg",
+        imageLarge: "http://localhost:3000/highlander_large.jpg",
+        price: "3200€",
+      },
+      {
+        id: 8,
+        name: "Prestige",
+        imageSmall: "http://localhost:3000/prestige_small.jpg",
+        imageLarge: "http://localhost:3000/prestige_large.jpg",
+        price: "9800€",
+      },
+    ]);
+  }, 3000);
+});
 
-app.get('/new-arrivals', (req, res) => {
+app.get("/new-arrivals", (req, res) => {
   res.json([
-    { id: 3, "name": "Boëme", "image": "http://localhost:3000/boeme.jpg", "price": "3000€" },
-    { id: 9, "name": "Gatsby", "image": "http://localhost:3000/gatsby.jpg", "price": "4700€" },
-    { id: 2, "name": "Nuage", "image": "http://localhost:3000/nuage.jpg", "price": "700€" },
-    { id: 4, "name": "Obsidian", "image": "http://localhost:3000/obsidian.jpg", "price": "5320€" },
-  ])
-})
+    {
+      id: 3,
+      name: "Boëme",
+      imageSmall: "http://localhost:3000/boeme_small.jpg",
+      imageLarge: "http://localhost:3000/boeme_large.jpg",
+      price: "3000€",
+    },
+    {
+      id: 9,
+      name: "Gatsby",
+      imageSmall: "http://localhost:3000/gatsby_small.jpg",
+      imageLarge: "http://localhost:3000/gatsby_large.jpg",
+      price: "4700€",
+    },
+    {
+      id: 2,
+      name: "Nuage",
+      imageSmall: "http://localhost:3000/nuage_small.jpg",
+      imageLarge: "http://localhost:3000/nuage_large.jpg",
+      price: "700€",
+    },
+    {
+      id: 4,
+      name: "Obsidian",
+      imageSmall: "http://localhost:3000/obsidian_small.jpg",
+      imageLarge: "http://localhost:3000/obsidian_large.jpg",
+      price: "5320€",
+    },
+  ]);
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
