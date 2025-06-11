41821175c213162579749b6d54bd7ce6dabb109a
Louis Lebrault <louis.lebrault@gmail.com>
2025-06-11 17:03:12 +0200

    ensure refunds page error is not preventing the app from building

--- app/refunds/page.js
+++ app/refunds/page.js
@@ -1,3 +1,8 @@
 export default function Refunds () {
-  throw new Error('A very unexpected error occured...');
+  // ensure this example error does not prevent the app from building
+  if (process.env.NODE_ENV === 'development') {
+    throw new Error('A very unexpected error occured...');
+  }
+
+  return null
 }
